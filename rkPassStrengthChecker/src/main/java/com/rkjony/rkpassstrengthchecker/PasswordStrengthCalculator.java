b49d065f1bb9dd5e2da82789427672c4d5edf33a
rk.jony <rezaulkarim.imraan@gmail.com>
2022-03-21 11:08:41 +0600

    initial commit

---
+++ rkPassStrengthChecker/src/main/java/com/rkjony/rkpassstrengthchecker/PasswordStrengthCalculator.java
@@ -0,0 +1,38 @@
+package com.rkjony.rkpassstrengthchecker;
+
+/**
+ * Created by gustavaaro on 2018-10-04.
+ */
+
+
+public interface PasswordStrengthCalculator {
+
+    /**
+     * Calculates the current password strength.
+     * @param password the password entered by the user
+     * @return an int in the range [0-N], where n is the number of defined security levels.
+     */
+    int calculatePasswordSecurityLevel(String password);
+
+
+    /**
+     * Defines the minimum length of a password
+     * @return An integer value which represents the minimum length of a password.
+     */
+    int getMinimumLength();
+
+
+    /**
+     * Defines what minimum security level is required for an accepted password.
+     * @param level the current security level of the password.
+     */
+
+    boolean passwordAccepted(int level);
+
+    /**
+     * Is called when the password is accepted.
+     * @param password the current password.
+     */
+    void onPasswordAccepted(String password);
+
+}
---
+++ rkPassStrengthChecker/src/main/java/com/rkjony/rkpassstrengthchecker/PasswordStrengthLevel.java
@@ -0,0 +1,24 @@
+package com.rkjony.rkpassstrengthchecker;
+
+/**
+ * Created by gustavaaro on 2018-10-04.
+ */
+
+public class PasswordStrengthLevel {
+
+    private int indicatorColor;
+    private String displayName;
+
+    public PasswordStrengthLevel(String displayName, int indicatorColor){
+        this.indicatorColor = indicatorColor;
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public int getIndicatorColor() {
+        return indicatorColor;
+    }
+}
